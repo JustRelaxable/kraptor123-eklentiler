497d9db3482dbdb1a567f5628e95168db2a35ce0
Taha Sökmen <31169187+JustRelaxable@users.noreply.github.com>
2025-07-29 18:43:22 +0300

    Update 4KFilmIzlesene.kt

--- 4KFilmIzlesene/src/main/kotlin/com/nikyokki/4KFilmIzlesene.kt
+++ 4KFilmIzlesene/src/main/kotlin/com/nikyokki/4KFilmIzlesene.kt
@@ -1,8 +1,6 @@
-
-
 package com.nikyokki
 
-import android.util.Log
+import com.lagradost.api.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
